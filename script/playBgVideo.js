34f41aab4051fb513c5e8f58cda3f6b3e55bb2e3
guaong <597384579@qq.com>
2017-11-25 15:27:48 +0800

    13

---
+++ script/playBgVideo.js
@@ -0,0 +1,63 @@
+//加载背景
+function loadBg(){
+  var winHeight = document.documentElement.clientHeight;
+  var winWidth = document.documentElement.clientWidth;
+  var dotline = new Dotline({
+    dom:'bg',//画布id
+    cw:winWidth,//画布宽
+    ch:winHeight,//画布高
+    ds:100,//点的个数
+    r:0.5,//圆点半径
+    dis:80//触发连线的距离
+  }).start();
+}
+//视频加载完成
+function loadComplete(){
+  canPlay = true;
+}
+//是否是pc
+function isPc(){
+  var userAgentInfo = navigator.userAgent;
+  var agents = new Array("Android", "iPhone", "Windows Phone", "iPad", "iPod");
+  for(var i=0; i < agents.length;i++){
+    if(userAgentInfo.indexOf(agents[i]) > 0){
+      return false
+    }
+  }
+  return true;
+}
+//播放视频
+function playVideo(){
+  if (canPlay) {
+    $("video").css("display", "block");
+    $("#bg").fadeOut(500);
+    text = $("#text");
+    $("#play").fadeOut(500);
+    $("#text").fadeOut(500);
+    document.getElementById('video').play();
+    timing = setInterval(showNext,15000);
+    isStart = true;
+    isVideoStop();
+  }
+}
+//判断视频播放完
+function isVideoStop(){
+  var video=document.getElementById("video");
+  video.addEventListener("ended",function(){
+      $('video').hide();
+      $("#bg").fadeIn(500);
+  });
+}
+//显示下一行字
+function showNext(){
+  text.text(content[i]);
+  text.fadeIn(1000);
+  i++;
+  if(i >= content.length){
+    clearInterval(timing);
+    $("#content").fadeIn(500);
+    text.animate({fontSize:'105px', fontWeight:'900'}, 1000);
+    return;
+  }
+  text.delay(5000).fadeOut(1000);
+}
